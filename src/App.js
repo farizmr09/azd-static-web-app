18d9a04f2c88135a95679a8e85187af622bebad8
farizmr09 <farizmuhridwan@gmail.com>
2023-03-21 11:41:39 +0700

    Minor adjustment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>Hello {value}, my name is Fariz Muhammad from University of Gadjah Mada</div>;
 }
 
 export default App;
